8864e2bfd589174e498faa27d9a14138087905b2
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-12-26 02:37:11 +0000

    little css try not sucessful

--- components/TasksList.js
+++ components/TasksList.js
@@ -3,19 +3,21 @@ import TaskItem from "./TaskItem";
 
 const TasksList = ({ tasks, onMarkCompleted, onEdit, onDelete }) => {
   return (
-    <ul className="list-disc pl-8 mt-4">
-      {tasks &&
-        tasks.length > 0 &&
-        tasks.map((task) => (
-          <TaskItem
-            key={task._id}
-            task={task}
-            onMarkCompleted={() => onMarkCompleted(task._id)}
-            onEdit={() => onEdit(task._id)}
-            onDelete={() => onDelete(task._id)}
-          />
-        ))}
-    </ul>
+    <div className="container mx-auto p-4 bg-white rounded-lg shadow-md">
+      <ul className="grid gap-4">
+        {tasks &&
+          tasks.length > 0 &&
+          tasks.map((task) => (
+            <TaskItem
+              key={task._id}
+              task={task}
+              onMarkCompleted={() => onMarkCompleted(task._id)}
+              onEdit={() => onEdit(task._id)}
+              onDelete={() => onDelete(task._id)}
+            />
+          ))}
+      </ul>
+    </div>
   );
 };
 
--- pages/completed.js
+++ pages/completed.js
@@ -7,10 +7,12 @@ const CompletedTasks = ({ fetchedTasks }) => {
   return (
     <div className="container mx-auto p-4 bg-white rounded-lg shadow-md">
       <h1 className="text-3xl font-bold mb-4">Completed Tasks</h1>
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <ul className="grid gap-4">
         {completedTasks.map((task) => (
-          <li key={task._id} className="bg-gray-100 rounded-lg p-4 shadow-md">
-            <h2 className="text-lg font-medium mb-2">{task.text}</h2>
+          <li key={task._id} className="bg-gray-100 rounded-lg p-4 shadow-md ">
+            <h2 className="text-lg font-medium mb-2 overflow-ellipsis overflow-hidden">
+              {task.text}
+            </h2>
           </li>
         ))}
       </ul>
